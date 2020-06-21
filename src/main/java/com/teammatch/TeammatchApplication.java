ae335768eb579e790de07a5b5f2603dc201a8cb8
Juanca0312 <39997847+Juanca0312@users.noreply.github.com>
2020-06-20 19:20:36 -0500

    Creacion de Proyecto
    Creacion de proyecto

---
+++ src/main/java/com/teammatch/TeammatchApplication.java
@@ -0,0 +1,13 @@
+package com.teammatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeammatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeammatchApplication.class, args);
+    }
+
+}
