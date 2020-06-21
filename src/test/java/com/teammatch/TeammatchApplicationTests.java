d5d849114a1f2fabbef2aa54bcb776f2837e329c
Juanca0312 <39997847+Juanca0312@users.noreply.github.com>
2020-06-20 19:20:36 -0500

    Creacion de Proyecto
    Creacion de proyecto

---
+++ src/test/java/com/teammatch/TeammatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teammatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeammatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
