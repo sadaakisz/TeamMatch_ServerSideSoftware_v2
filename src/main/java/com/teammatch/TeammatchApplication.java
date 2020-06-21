2fddde8e255b8c3ee0ecbe0e51576a11afa08d93
Juanca0312 <39997847+Juanca0312@users.noreply.github.com>
2020-06-20 19:34:38 -0500

    Packages creation

--- src/main/java/com/teammatch/TeammatchApplication.java
+++ src/main/java/com/teammatch/TeammatchApplication.java
@@ -1,13 +1,21 @@
 package com.teammatch;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TeammatchApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TeammatchApplication.class, args);
     }
 
+    @Bean //agregar
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
