8d263eadcb6e452771323e2fbc6be004f32c3ce0
Juanca0312 <39997847+Juanca0312@users.noreply.github.com>
2020-07-01 21:19:39 -0500

    Message creation

---
+++ src/main/java/com/teammatch/controller/MessageController.java
@@ -0,0 +1,79 @@
+package com.teammatch.controller;
+
+import com.teammatch.model.Message;
+import com.teammatch.resource.MessageResource;
+import com.teammatch.resource.SaveMessageResource;
+import com.teammatch.service.MessageService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api")
+public class MessageController {
+
+    @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
+    private MessageService messageService;
+
+    @GetMapping("/messages")
+    public List<Message> getAllMessages() {
+        return messageService.getAllMessages();
+    }
+
+    @GetMapping("/chats/{chatId}/messages")
+    public Page<MessageResource> getAllChatsByUserId(
+            @PathVariable(name = "chatId") Long chatId,
+            Pageable pageable)
+    {
+        Page<Message> messagePage = messageService.getAllMessagesByChatId(chatId, pageable);
+        List<MessageResource> resources = messagePage.getContent().stream().map(this::convertToResource).collect(Collectors.toList());
+        return new PageImpl<>(resources, pageable, resources.size());
+    }
+
+    @GetMapping("/chats/{chatId}/messages/{messageId}")
+    public MessageResource getMessageByIdAndChatId(@PathVariable(name = "chatId") Long chatId,
+                                                     @PathVariable(name = "messageId") Long messageId){
+        return convertToResource(messageService.getChatByIdAndChatId(chatId, messageId));
+    }
+
+    @PostMapping("/chats/{chatId}/messages")
+    public MessageResource createMessage(@PathVariable(name = "chatId") Long chatId,
+                                           @Valid @RequestBody SaveMessageResource resource){
+        return convertToResource(messageService.createMessage(chatId, convertToEntity(resource)));
+    }
+
+    @PutMapping("/chats/{chatId}/messages/{messageId}")
+    public MessageResource updateMessage(@PathVariable(name = "chatId") Long chatId,
+                                           @PathVariable(name = "messageId") Long messageId,
+                                           @Valid @RequestBody SaveMessageResource resource){
+        return convertToResource(messageService.updateMessage(chatId, messageId, convertToEntity(resource)));
+    }
+
+    @DeleteMapping("/chats/{chatId}/messages/{messageId}")
+    public ResponseEntity<?> deleteMessage(@PathVariable(name = "chatId") Long chatId,
+                                            @PathVariable(name="messageId") Long messageId){
+        return  messageService.deleteMessage(chatId, messageId);
+    }
+
+    private Message convertToEntity(SaveMessageResource resource) {
+        return mapper.map(resource, Message.class);
+    }
+
+    private MessageResource convertToResource(Message entity) {
+        return mapper.map(entity, MessageResource.class);
+    }
+
+
+
+}
