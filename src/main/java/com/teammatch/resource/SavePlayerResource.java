60c5b7deb506cd536d17777f693e4ce3e762c046
Juanca0312 <jchernandez0312@gmail.com>
2020-10-15 20:39:31 -0500

    arreglado el bug

--- src/main/java/com/teammatch/resource/SavePlayerResource.java
+++ src/main/java/com/teammatch/resource/SavePlayerResource.java
@@ -1,9 +1,12 @@
 package com.teammatch.resource;
 
+import lombok.Data;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+@Data
 public class SavePlayerResource {
     @NotBlank
     @NotNull
