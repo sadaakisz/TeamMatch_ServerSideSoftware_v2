5a913a8fe851b87315b9adcdbcf41fbb3c024f72
Juan Alonso <leyva.calle1403@gmail.com>
2021-05-06 14:56:22 -0500

    fix

--- src/test/java/com/teammatch/TeammatchApplicationTests.java
+++ src/test/java/com/teammatch/TeammatchApplicationTests.java
@@ -6,7 +6,9 @@ import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.RunWith;
 import org.junit.runner.notification.Failure;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"pretty"},features = {"classpath:features/"},
         glue = {"com.teammatch"})
