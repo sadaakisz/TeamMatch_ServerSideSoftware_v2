0dc5d540453ca2262b6c73d29dc473d84f0aaf69
Juan Alonso <leyva.calle1403@gmail.com>
2021-05-31 18:17:22 -0500

    Some bugs fixed

---
+++ src/test/java/com/teammatch/TeammatchApplicationTests.java
@@ -0,0 +1,12 @@
+package com.teammatch;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TeammatchApplicationTests {
+    @Test
+    public void contextLoads() {
+    }
+}
